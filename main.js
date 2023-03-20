1f6a4a8f4d75dc2b71422652a391b987e184401b
Hidzero <68088463+Hidzero@users.noreply.github.com>
2023-03-20 20:40:19 -0300

    add files

---
+++ main.js
@@ -0,0 +1,30 @@
+// function criarPessoa(nome, sobrenome){
+//     return{
+//         nome, sobrenome
+//     };
+// }
+
+// const p1 = criarPessoa("Fabricio", "Dias");
+
+
+
+// // evento
+
+// document.addEventListener('click', ()=>{
+//     document.body.style.backgroundColor = 'blue';
+// });
+
+//pesquisar sobre a DOM
+//pesquisar sobre os addEventListener
+//foco princial em como funciona a manipulação pelo document
+
+//vão criar um codigo de exemplo utilizando addeventlistner
+
+// O DOM é uma interface de programação que os navegadores usam para representar 
+// paginas web sendo usado em um modelo de representação e interação do documento HTML ou XML
+
+// é estruturado em formato de arvore
+
+var titulo = document.getElementsByTagName('h1')[0];
+console.log(titulo);
+
