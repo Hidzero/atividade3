aeeebc4852ea9e96755ba9698e96eac54b540dbe
Hidzero <68088463+Hidzero@users.noreply.github.com>
2023-03-20 21:01:54 -0300

    finalizado

--- main.js
+++ main.js
@@ -1,33 +1,19 @@
-// function criarPessoa(nome, sobrenome){
-//     return{
-//         nome, sobrenome
-//     };
-// }
-
-// const p1 = criarPessoa("Fabricio", "Dias");
-
-
-
-// // evento
-
-// document.addEventListener('click', ()=>{
-//     document.body.style.backgroundColor = 'blue';
-// });
-
-//pesquisar sobre a DOM
-//pesquisar sobre os addEventListener
-//foco princial em como funciona a manipulação pelo document
-
-//vão criar um codigo de exemplo utilizando addeventlistner
-
-// O DOM é uma interface de programação que os navegadores usam para representar 
-// paginas web sendo usado em um modelo de representação e interação do documento HTML ou XML
-
-// é estruturado em formato de arvore
-
-// var titulo = document.getElementsByTagName('h1')[0];
-// console.log(titulo);
-
-function a(){
+function amo(){
     alert('Eu te amo S2')
 }
+
+const botao = document.querySelector('#elemento');
+
+function clicou(){
+    botao.disabled = true
+}
+
+const elemento = document.getElementById('elemento');
+
+elemento.addEventListener('mouseover', () => {
+elemento.style.opacity = '0';
+});
+
+elemento.addEventListener('mouseout', () => {
+elemento.style.opacity = '1';
+});
